1989cb1b2c843cbe3a81f882bba628849ea4fe2a
Siddhartha Nihalani <narenana@gmail.com>
2011-09-30 16:44:37 +0530

    added custom config

--- dompdf_config.custom.inc.php
+++ dompdf_config.custom.inc.php
@@ -7,9 +7,9 @@
 //define("DOMPDF_DEFAULT_MEDIA_TYPE", "print");
 //define("DOMPDF_DEFAULT_PAPER_SIZE", "letter");
 //define("DOMPDF_DEFAULT_FONT", "serif");
-//define("DOMPDF_DPI", 72);
-//define("DOMPDF_ENABLE_PHP", true);
-//define("DOMPDF_ENABLE_REMOTE", true);
+define("DOMPDF_DPI", 300);
+define("DOMPDF_ENABLE_PHP", true);
+define("DOMPDF_ENABLE_REMOTE", true);
 //define("DOMPDF_ENABLE_CSS_FLOAT", true);
 //define("DOMPDF_ENABLE_JAVASCRIPT", false);
 //define("DEBUGPNG", true);
